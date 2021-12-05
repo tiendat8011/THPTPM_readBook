a7f1b057afa17e8cad11af55ba1b1d7941afdb38
Dat <ntd8011@gmail.com>
2021-12-05 22:52:42 +0700

    update book api

--- routes/controllers/books/index.js
+++ routes/controllers/books/index.js
@@ -3,12 +3,12 @@ const bookController = require('./books');
 const router = express.Router();
 const { authenticate, authorize } = require('../../../middlewares/auth');
 
-router.get('/',/* authenticate, authorize('admin'),*/ bookController.getBook);
-router.get('/detail-book/:id',/* authenticate, authorize('admin'),*/ bookController.getBookById);
+router.get('/', authenticate,  bookController.getBook);
+router.get('/detail-book/:id', authenticate, bookController.getBookById);
 
 router.post('/',  authenticate, authorize('author'), bookController.createBook)
 
-router.put('/:id',/* authenticate, authorize('admin'),*/ bookController.updateBookById);
+router.put('/:id', authenticate, authorize('admin author'), bookController.updateBookById);
 
 router.delete('/:id', authenticate, authorize('admin author'), bookController.deleteBookById);
 
