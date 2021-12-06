a81201fb67785b9bd5e9dbab74b0033932e571aa
Dat <ntd8011@gmail.com>
2021-12-06 19:50:58 +0700

    category api

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const userRouter = require('./controllers/users/index');
 const bookRouter = require('./controllers/books/index')
+const categoryRouter = require('./controllers/category/index')
 const router = express.Router();
 
 router.use('/users', userRouter);
 router.use('/books', bookRouter);
+router.use('/category', categoryRouter);
+
 
 
 module.exports = router;
